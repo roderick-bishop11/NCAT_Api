4195fbe779aeb0295f28f2e037bd1a3960517e45
Roderick Bishop <rbbishop11@gmail.com>
2023-03-24 05:04:53 -0500

    Update main.py
    adding a method

--- Python/main.py
+++ Python/main.py
@@ -8,10 +8,11 @@ def home_page():
     return "Hello World"
 
 
-@app.route('/double', methods=['GET'])
+@app.route('/double', methods=['POST'])
 def double():
-    query_val = int(request.args.get('value'))
-    return f"Previous number was {query_val} the new value is {(query_val * 2)}"
+    query_val = request.json['value']
+    return f"Previous number was {query_val} the new value is {(int(query_val) * 2)}"
+
 
 if __name__ == '__main__':
     app.run(port=8080)
